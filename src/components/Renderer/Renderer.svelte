51e17952d3d9546666525bdd705456db233c7d92
TheWisePigeon <zozozozeph@gmail.com>
2023-06-05 22:24:29 +0000

    finishing POC

--- src/components/Renderer/Renderer.svelte
+++ src/components/Renderer/Renderer.svelte
@@ -1,5 +1,26 @@
 <script lang="ts">
-    export let data :{id:string,value:string}[]
+	import Header from '../Header/Header.svelte';
+	import TextBox from '../TextBox/TextBox.svelte';
+    import type { SvelteComponent } from 'svelte';
+	export let data: { id: string; value: string }[];
+	const componentMap: Record<string, typeof SvelteComponent> = {
+		'Header': Header,
+		'TextBox': TextBox
+	};
+	function render_component() {
+		return data.map((component) => {
+			const Component = componentMap[component.id];
+			return {
+				Component,
+				props: {
+					content: component.value
+				}
+			};
+		});
+	}
 </script>
 
-<h1>I am the renderer</h1>
+
+{#each render_component() as { Component, props }}
+    <svelte:component this={Component} content={props.content} />
+{/each}
--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
-    import Renderer from "../../components/Renderer/Renderer.svelte";
+	import Renderer from '../../components/Renderer/Renderer.svelte';
+	import type { PaperComponent } from '$lib/types';
+	const data: PaperComponent[] = [
+		{
+			id: 'Header',
+			value: 'Hey I am a header'
+		},
+		{
+			id: 'TextBox',
+			value: 'This API helps you find your cat lost on the internet'
+		}
+	];
 </script>
 
-
-<div class=" m-auto w-[50%] border-x-4 border-black h-full p-2 ">
-    <Renderer data={ [{ id:"1", value:"Welcome to Paperss" }] } />
+<div class=" m-auto w-[50%] border-x-4 border-black h-full p-2">
+	<Renderer {data} />
 </div>
-
